68b51f511f10d688ee19cc611b18f4fbfd729289
Oliver Liam Kelly <oliverliamkelly@gmail.com>
2022-03-05 23:34:59 +1100

    turning menufunction choices into functions

--- app.js
+++ app.js
@@ -32,11 +32,46 @@ const queryUpdateRole = 'UPDATE roles SET ? WHERE ?;';
 
 const queryEmploysInfo = 'SELECT employees.id, employees.firstName, employees.lastName, roles.title, roles.salary, departments.department FROM employees LEFT JOIN roles ON employees.roleId = roles.id LEFT JOIN departments ON roles.deptId = departments.id;';
 const queryRolesInfo = 'SELECT roles.id, roles.title, roles.salary, departments.department FROM roles LEFT JOIN departments ON roles.deptId = departments.id;';
+const queryAddEmployHelp = `SELECT employees.first_name, employees.last_name, employees.role_id, roles.title FROM employees INNER JOIN roles ON employees.role_id = roles.id WHERE employees.is_mangr=TRUE;`;
 
 function getRoles(){
     connection.query(queryRoles, (err, res) => {
         if(err) throw err;
         console.table(res);
+    });
+}
+
+function newRole(){
+    connection.query(queryDepart, (err, res) => {
+        let departChoice = res.map(function (res) {
+            return res['depart'];
+        });
+        prompt([
+            { type: 'input', name: 'newRoleTitle', message: 'What is the name of this new Role?' },
+            { type: 'input', name: 'newRoleSalary', message: 'What is the salary of this new Role?' },
+            {
+                type: 'list',
+                name: 'newRoleDepart',
+                message: 'Which Department is this new Role under?',
+                choices: departChoice
+            }
+        ])
+        .then((answers) => {
+            connection.query(queryDepart, (err, res) => {
+                if (err) throw err;
+                const departments = res.find(departments => departments.deptartment === answers.newRoleDepart);
+                connection.query(queryAddRole,
+                {
+                    title: answers.newRoleTitle,
+                    salary: answers.newRoleSalary,
+                    deptId: departments.id
+                }, (err, res) => {
+                    if (err) throw err;
+                    console.table(res); 
+                    getRoles();
+                });
+            });
+        });
     });
 }
 
@@ -64,37 +99,7 @@ async function menuFunct(){
                 menuFunct();
                 break;
             case 'Add New Role':
-                const newRole = connection.query(queryDepart, (err, res) => {
-                    let departChoice = res.map(function (res) {
-                        return res['dept'];
-                    });
-                    prompt([
-                        { type: 'input', name: 'newRoleTitle', message: 'Name of New Role?' },
-                        { type: 'input', name: 'newRoleSalary', message: 'Salary of New Role?' },
-                        {
-                            type: 'list',
-                            name: 'newRoleDept',
-                            message: 'Which department is this New Role under?',
-                            choices: departChoice
-                        }
-                    ])
-                    .then((answers) => {
-                        connection.query(queryDepart, (err, res) => {
-                            if (err) throw err;
-                            const departments = res.find(departments => departments.deptartment === answers.newRoleDepart);
-                            connection.query(queryAddRole,
-                            {
-                                title: answers.newRoleTitle,
-                                salary: answers.newRoleSalary,
-                                deptId: departments.id
-                            }, (err, res) => {
-                                if (err) throw err;
-                                console.table(res);
-                                getRoles();
-                            });
-                        });
-                    });
-                });
+                newRole(); 
                 menuFunct();               
                 break;
             case 'View All Departments':
